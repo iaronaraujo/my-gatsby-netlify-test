e1b5a6cacb476a758f22e4f449714a3f072d4945
iaron <iaron.araujo@vtex.com.br>
2020-11-10 11:21:06 -0300

    First commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
-}
+  return (
+    <div style={{ color: `purple`, fontSize: `72px` }}>
+      <h1>Hello VTEX!!!</h1>
+      <p>More info!</p>
+      <div>
+        <Link to="/about">About</Link>
+      </div>
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+    ) 
+  }
